091562a11231d94e6ca2b7b93e670001eebbdebd
Tianqi <airstone42@gmail.com>
2019-04-06 11:27:07 +0800

    feat(examples): add tests
    add tests for different status

---
+++ examples/test_2.cpp
@@ -0,0 +1,14 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+int main() {
+    this_thread::sleep_for(seconds(1));
+    int a = 0, b = 0;
+    while (cin >> a >> b)
+        cout << a + b << endl;
+    return 0;
+}
